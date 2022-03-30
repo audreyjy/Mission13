9e8ff82f3b2cd6534d5aa4e341c68a2766518f4f
audreyjy <audrey.youngberg1@gmail.com>
2022-03-30 10:49:11 -0600

    Add project files.

---
+++ Mission13/Models/BowlingDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; 
+
+namespace Mission13.Models
+{
+    public class BowlingDbContext : DbContext
+    {
+        public BowlingDbContext(DbContextOptions<BowlingDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Bowler> Bowlers { get; set; }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        optionsBuilder.UseMySql("server=localhost; port=3306; database=bowlingleagueexample; user=root; password=goldfish!32",
+        //            builder => builder.EnableRetryOnFailure());
+
+        //    }
+        //}
+
+    }
+}
